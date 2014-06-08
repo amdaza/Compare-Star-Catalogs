7775a0ca875b563f02a65a245307417c0d0f8545
Alicia Daza <amdazacastillo@gmail.com>
2014-06-08 15:47:26 +0200

    Memory changes

--- CompareCatalogs/src/view/String_Type.java
+++ CompareCatalogs/src/view/String_Type.java
@@ -2,23 +2,24 @@ package view;
 
 public class String_Type {
 	private String s;
-	private view.DataStructure.Type t;
-	
-	
+	private view.DataStructure.Type t;	
 	
 	public String_Type(String s, view.DataStructure.Type t) {
 		this.s = s;
 		this.t = t;
 	}
+	
 	public String getS() {
 		return s;
 	}
+	
 	public void setS(String s) {
 		this.s = s;
 	}
 	public view.DataStructure.Type getT() {
 		return t;
 	}
+	
 	public void setT(view.DataStructure.Type t) {
 		this.t = t;
 	}
